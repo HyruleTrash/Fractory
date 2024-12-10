e6ddc1bce89ec8dc11be9dbf8d6fa7113ac86974
TrashElf <83176601+HyruleTrash@users.noreply.github.com>
2024-12-10 19:54:52 +0100

    update: managed to convert the 6 year old shader video to hlsl + unity 6 urp

--- Assets/Art/Shaders/RaymarchingShader.shader
+++ Assets/Art/Shaders/RaymarchingShader.shader
@@ -1,63 +1,76 @@
 Shader "FracturedRealm/RaymarchingShader"
 {
-    Properties
+    HLSLINCLUDE
+    
+    #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+    // The Blit.hlsl file provides the vertex shader (Vert),
+    // the input structure (Attributes), and the output structure (Varyings)
+    #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+
+    float4 _CamWorldSpace;
+    float4x4 _CamFrustum, _CamToWorld;
+
+    struct AttributesCustom
     {
-        _MainTex ("Texture", 2D) = "white" {}
+        uint vertexID : SV_VertexID;
+        float4 vertex : POSITION;
+        float2 uv : TEXCOORD0;
+        UNITY_VERTEX_INPUT_INSTANCE_ID
+    };
+
+    struct VaryingsCustom
+    {
+        float2 uv : TEXCOORD0;
+        float4 vertex : SV_POSITION;
+        float3 ray : TEXCOORD1;
+        UNITY_VERTEX_OUTPUT_STEREO
+    };
+
+    VaryingsCustom Vert(AttributesCustom input)
+    {
+        VaryingsCustom output;
+        UNITY_SETUP_INSTANCE_ID(input);
+        UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+
+        float4 pos = GetFullScreenTriangleVertexPosition(input.vertexID);
+        output.uv = GetFullScreenTriangleTexCoord(input.vertexID);
+
+        output.vertex = pos;
+
+        output.ray = _CamFrustum[(int)input.vertexID].xyz;
+
+        output.ray /= abs(output.ray.z);
+
+        output.ray = mul(_CamToWorld, output.ray);
+
+        return output;
     }
+
+    float4 Frag (VaryingsCustom input) : SV_Target
+    {
+        //return float4(input.uv.xy, 1, 1);
+        float3 rayDir = normalize(input.ray.xyz);
+        float3 rayOrigin = _CamWorldSpace.xyz;
+        return float4(rayDir, 1);
+    }
+
+    ENDHLSL
     SubShader
     {
-        Cull Off ZWrite Off ZTest Always
-
+        Tags { "RenderType"="Opaque" "RenderPipeline" = "UniversalPipeline"}
+        LOD 100
+        ZWrite Off Cull Off
         Pass
         {
-            CGPROGRAM
-            #pragma vertex vert
-            #pragma fragment frag
-            #pragma target 3.0
+            Name "RaymarchingPass"
 
-            #include "UnityCG.cginc"
+            HLSLPROGRAM
             
-            sampler2D _MainTex;
-            uniform float4 _CamWorldSpace;
-            uniform float4x4 _CamFrustum, _CamToWorld;
-
-            struct appdata
-            {
-                float4 vertex : POSITION;
-                float2 uv : TEXCOORD0;
-            };
-
-            struct v2f
-            {
-                float2 uv : TEXCOORD0;
-                float4 vertex : SV_POSITION;
-                float3 ray : TEXCOORD1;
-            };
-
-            v2f vert (appdata v)
-            {
-                v2f o;
-                half index = v.vertex.z;
-                v.vertex.z = 0;
-                o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = v.uv;
-
-                o.ray = _CamFrustum[(int)index].xyz;
-
-                o.ray /= abs(o.ray.z);
-
-                o.ray = mul(_CamToWorld, o.ray);
-
-                return o;
-            }
-
-            fixed4 frag (v2f i) : SV_Target
-            {
-                float3 rayDir = normalize(i.ray.xyz);
-                float3 rayOrigin = _CamWorldSpace.xyz;
-                return fixed4(rayDir, 1);
-            }
-            ENDCG
+            #pragma vertex Vert
+            #pragma fragment Frag
+            #pragma target 3.0
+            
+            ENDHLSL
         }
     }
 }
--- Assets/Scripts/Rendering/RaymarchRendererFeature.cs
+++ Assets/Scripts/Rendering/RaymarchRendererFeature.cs
@@ -1,36 +1,112 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.RenderGraphModule.Util;
 using UnityEngine.Rendering.Universal;
 
 public class RaymarchRendererFeature : ScriptableRendererFeature
 {
     public Shader shader;
-    private Material _raymarchMaterial;
     private RaymarchPass _raymarchPass;
+    private Material material;
 
     public override void Create()
     {
         if (shader == null)
             return;
 
-        _raymarchMaterial = CoreUtils.CreateEngineMaterial(shader);
-        _raymarchPass = new RaymarchPass(_raymarchMaterial)
-        {
-            renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing
-        };
+        material = new Material(shader);
+        _raymarchPass = new RaymarchPass(material);
+
+        _raymarchPass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
-        if (_raymarchMaterial == null)
-            return;
-
-        _raymarchPass.Setup(renderingData.cameraData.camera);
         renderer.EnqueuePass(_raymarchPass);
     }
 
     protected override void Dispose(bool disposing)
     {
-        CoreUtils.Destroy(_raymarchMaterial);
+        if (Application.isPlaying)
+        {
+            Destroy(material);
+        }
+        else
+        {
+            DestroyImmediate(material);
+        }
+    }
+
+
+    public class RaymarchPass : ScriptableRenderPass{
+        private Camera cam;
+        private Material _raymarchMaterial;
+        TextureHandle _source, destination;
+        private RenderTextureDescriptor textureDescriptor;
+
+        public RaymarchPass(Material material)
+        {
+            _raymarchMaterial = material;
+            textureDescriptor = new RenderTextureDescriptor(Screen.width, Screen.height, RenderTextureFormat.Default, 0);
+            cam = Camera.main;
+        }
+
+        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData){
+            UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
+            UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+
+            // Set the blur texture size to be the same as the camera target size.
+            textureDescriptor.width = cameraData.cameraTargetDescriptor.width;
+            textureDescriptor.height = cameraData.cameraTargetDescriptor.height;
+            textureDescriptor.depthBufferBits = 0;
+
+            _source = resourceData.activeColorTexture;
+            destination = UniversalRenderer.CreateRenderGraphTexture(renderGraph, textureDescriptor, "RaymarchPass_tex", false);
+
+            SetSettings();
+            
+            RenderGraphUtils.BlitMaterialParameters output = new(destination, _source, _raymarchMaterial, 0);
+            renderGraph.AddBlitPass(output, "RaymarchPass");
+        }
+
+        private void SetSettings(){
+            if (!_raymarchMaterial)
+            {
+                return;
+            }
+
+            if (cam == null)
+            {
+                Debug.LogError("Main camera not found");
+                return;
+            }
+
+            _raymarchMaterial.SetMatrix("_CamFrustum", CamFrustum(cam));
+            _raymarchMaterial.SetMatrix("_CamToWorld", cam.cameraToWorldMatrix);
+            _raymarchMaterial.SetVector("_CamWorldPos", cam.transform.position);
+        }
+
+        private Matrix4x4 CamFrustum(Camera cam)
+        {
+            Matrix4x4 frustum = Matrix4x4.identity;
+            float fov = Mathf.Tan((cam.fieldOfView * 0.5f) * Mathf.Deg2Rad);
+
+            Vector3 goUp = Vector3.up * fov;
+            Vector3 goRight = Vector3.right * fov * cam.aspect;
+
+            Vector3 TL = -Vector3.forward - goRight + goUp;
+            Vector3 TR = -Vector3.forward + goRight + goUp;
+            Vector3 BR = -Vector3.forward + goRight - goUp;
+            Vector3 BL = -Vector3.forward - goRight - goUp;
+
+            frustum.SetRow(0, TL);
+            frustum.SetRow(1, TR);
+            frustum.SetRow(2, BR);
+            frustum.SetRow(3, BL);
+
+            return frustum;
+        }
     }
 }
